63465f481c843946aaa085e0c4a321b3b6d32fca
Abraham Juárez <AjSux@outlook.com>
2020-06-17 18:39:31 -0500

    Nombre del Proyecto: Spring Security
    Descripción: Se agrega el contexto Maven inicial para empezar el proyecto de Spring security.

---
+++ src/main/java/io/spring/security/sux/springbootsecuritycourse/SpringBootSecurityCourseApplication.java
@@ -0,0 +1,13 @@
+package io.spring.security.sux.springbootsecuritycourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/spring/security/sux/springbootsecuritycourse/SpringBootSecurityCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.security.sux.springbootsecuritycourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
